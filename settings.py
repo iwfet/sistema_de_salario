12b7b1bf12e7105428e95e344279e262f6902ae3
iwfet <58945624+iwfet@users.noreply.github.com>
2021-05-20 15:13:50 -0300

    Add files via upload

--- settings.py
+++ settings.py
@@ -1,62 +1,57 @@
-from re import T
-import time 
+import time
 import tkinter as tk
-from tkinter import Canvas, filedialog
+from tkinter import Canvas, DoubleVar, Message, filedialog, ttk
+from tkinter.constants import X
 import pandas as pd
 import numpy  as np
 from openpyxl import *
+import pyautogui
 import Calculo_valores as calc
 
-
-
+#Criando as listas
 valor_salario_liquido = []
 valor_desconto_inss = []
 valor_desconto_irrf = []
 
+#Gerando parte visual
 root = tk.Tk()
+root.title('Selecionar arquivo')
+varBarra = DoubleVar()
+varBarra.set(0)
 
-Canvas1 = tk.Canvas(root, width= 300, height= 300, bg='lightsteelblue')
+Canvas1 = tk.Canvas(root, width= 300, height= 300, bg='white')
 Canvas1.pack()
+pb = ttk.Progressbar(root, variable=varBarra, maximum=100)
+pb.place(x=78, y=200, width=144, height= 20)   
+valor_salario_liquido = []
 
-lb = tk.Label(root,text = "")
-lb.place(x=125, y=200)
-lb.config(fg="lightsteelblue")
-lb["bg"] = "lightsteelblue"
+ler = 0
+import_file_path = filedialog.askopenfilename()
+ler = pd.read_excel(import_file_path)
 
-ler = 0  
-def mudaTxt():
-    lb["text"] = "Gerado"
-    lb["fg"] = "white"
-    lb["bg"] = "green"
-    
-    
-def getExcel():    
+def getExcel():
     global ler
-    import_file_path = filedialog.askopenfilename()
-    ler = pd.read_excel(import_file_path)
-    mudaTxt()
-    time.sleep(3)
-    browseButtonFechar['fg'] = 'red'
-
+    count = 0
+    etapas= 100
+    while count < etapas:
+        count= count + 1
+        i = 0
+        while i <1000000:
+            i = i + 3
+        varBarra.set(count)
+        root.update()
+    root.quit()
     
 browseButton_excel = tk.Button(
-    text="importe excel file",
-    command=getExcel, 
-    bg='green', fg='white', font=('helvetica', 12, 'bold'))
+    text="Executar",
+    command=getExcel,
+    bg='green', fg='black', font=('helvetica', 12, 'bold'))
 Canvas1.create_window(150,150, window=browseButton_excel)
-
-browseButtonFechar = tk.Button(
-text="Fechar",
-command=root.quit(),
-bg='lightsteelblue', fg='lightsteelblue', font=('helvetica', 12, 'bold'))
-Canvas1.create_window(150,250, window=browseButtonFechar)
-
 
 
 root.mainloop()
 
-
-
+#Iniciando o for junto com os calculos
 for index, linha in ler.iterrows():
     id = linha["Matricula"]
     nome = linha["Nome"]
@@ -64,19 +59,23 @@ for index, linha in ler.iterrows():
     Descontos = linha["Desconto"]
     Salario_bruto = linha["Salario"]
     
+    #Chamando as fuctions
     desconto_inss = calc.DescontoINSS(Salario_bruto)
     desconto_irrf = calc.DescontoIRRF(Salario_bruto,desconto_inss,Dependentes)
     valor_final = Salario_bruto - desconto_irrf - Descontos- desconto_inss
     
+    #Colocando os valores nas variaveis 
     valor_salario_liquido.append(f'''{valor_final:,.2f}''')
     valor_desconto_inss.append(f'''{desconto_inss:,.2f}''')
     valor_desconto_irrf.append(f'''{desconto_irrf:,.2f}''')
 
+#Adicionando as listas no excel
 ler['Desconto do inss'] = valor_desconto_inss
 ler['Desconto do irrf'] =  valor_desconto_irrf
 ler['Salario liquido'] = valor_salario_liquido
 
-writer = pd.ExcelWriter('salario_com_os_descontos.xlsx')
+#Gerando o excel
+writer = pd.ExcelWriter('Folha Descontada.xlsx')
 ler.to_excel(writer,'new_sheet')
 writer.save()
 print(ler)
